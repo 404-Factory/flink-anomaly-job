46b7b35f1906f1ac6377efe91680325b630e46e2
Suguen Lim <guen8903@gmail.com>
2026-06-11 19:27:47 +0900

    fix: ignore unknown JSON properties in SensorDataBatchDeserializer

--- src/main/java/com/factory/flink/serialization/SensorDataBatchDeserializer.java
+++ src/main/java/com/factory/flink/serialization/SensorDataBatchDeserializer.java
@@ -1,7 +1,9 @@
 package com.factory.flink.serialization;
 
 import com.factory.flink.dto.SensorDataBatchDto;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
 import java.io.IOException;
 
@@ -12,14 +14,20 @@ public class SensorDataBatchDeserializer extends AbstractDeserializationSchema<S
     @Override
     public void open(InitializationContext context) throws Exception {
         super.open(context);
-        objectMapper = new ObjectMapper();
+        objectMapper = buildMapper();
     }
 
     @Override
     public SensorDataBatchDto deserialize(byte[] message) throws IOException {
         if (objectMapper == null) {
-            objectMapper = new ObjectMapper();
+            objectMapper = buildMapper();
         }
         return objectMapper.readValue(message, SensorDataBatchDto.class);
+    }
+
+    private static ObjectMapper buildMapper() {
+        return new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 }
